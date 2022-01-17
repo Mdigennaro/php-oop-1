a06b4c8409e3039a02053b48adfec9cd7fd586a4
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-17 17:35:31 +0100

    aggiunta variabili d'istanza

--- index.php
+++ index.php
@@ -1,10 +1,32 @@
 <?php
 
+//Classe
 class Movie {
 
+  public $titolo;
+  public $genere;
+  public $durata;
+  public $anno;
+  public $valutazione;
 
 }
 
+//Variabili d'istanza
 $avatar = new Movie();
+$avatar->titolo = "Avatar";
+$avatar->genere = "Sci-fi/Azione";
+$avatar->durata = 162;
+$avatar->anno = 2009;
+$avatar->valutazione = 4.5;
+
+$interstellar = new Movie();
+$interstellar->titolo = "Interstellar";
+$interstellar->genere = "Sci-fi/Avventura";
+$interstellar->durata = 169;
+$interstellar->anno = 2014;
+$interstellar->valutazione = 3.5;
+
+var_dump($avatar);
+var_dump($interstellar);
 
 ?>

34a51590cf26d8f17ed371881ee2f35f3147c029
Mdigennaro <michele.digennaro719@gmail.com>
2022-01-17 17:57:20 +0100

    aggiunta metodo e costruttore

--- index.php
+++ index.php
@@ -9,6 +9,13 @@ class Movie {
   public $anno;
   public $voto = '';
 
+  //Costruttore
+  function __construct($_titolo)
+  {
+    $this->titolo = $_titolo;
+  }
+
+  //metodi
   public function setVoto($valutazione){
     if ($valutazione <= 2.5) {
       $this->voto = "bassa";
@@ -28,15 +35,13 @@ class Movie {
 }
 
 //Variabili d'istanza
-$avatar = new Movie();
-$avatar->titolo = "Avatar";
+$avatar = new Movie("Avatar");
 $avatar->genere = "Sci-fi/Azione";
 $avatar->durata = 162;
 $avatar->anno = 2009;
 $avatar->setVoto(4.5);
 
-$interstellar = new Movie();
-$interstellar->titolo = "Interstellar";
+$interstellar = new Movie("Interstellar");
 $interstellar->genere = "Sci-fi/Avventura";
 $interstellar->durata = 169;
 $interstellar->anno = 2014;
